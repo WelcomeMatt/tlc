bf937ab7b43321954d45d7020bda8531a3824a18
WelcomeMatt <mhart02@mail.uoguelph.ca>
2017-03-31 11:38:49 -0400

    rough files for tlc site. all pages minus contact and quote.

---
+++ tlc_rough/job-application.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>The Landscape Company</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/business-casual.css" rel="stylesheet">
+    <link href="css/index.css" rel="stylesheet">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div class="brand"><img src="img/logo.jpg" id="main-logo"/></div>
+    <div class="address-bar">Design | Construction | Maintenance</div>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <!-- navbar-brand is hidden on larger screens, but visible when the menu is collapsed -->
+                <a class="navbar-brand" href="index.html">The Landscape Company</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="index.html">Home</a>
+                    </li>
+                    <li>
+                        <a href="index.html#about_info">About</a>
+                    </li>
+                    <li>
+                        <a href="services.html">Services</a>
+                    </li>
+                    <li>
+                        <a href="work.html">Our Work</a>
+                    </li>
+                    <li>
+                        <a href="contact.html">Careers</a>
+                    </li>
+                    <li>
+                        <a href="contact.html">Request a Quote</a>
+                    </li>
+                    <li>
+                        <a href="contact.html">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">
+                        <strong>Careers at The Landscape Company</strong>
+                    </h2>
+                    <hr>
+                    <p>The Landscape Company are always looking for experienced and energetic men and women to joing our fast growing team. Please complete the form below and we will contact appropriate candidates.</p>
+                    <p>Fields marked in <span style="color:red">RED</span> must be completed.</p>
+                    <form>
+                        <div class="form-group">
+                            <label for="first-name">First Name:</label>
+                            <input type="text" class="form-control" id="first-name" style="width:50%" placeholder="First Name...">
+                        </div>
+                        <div class="form-group">
+                            <label for="last-name">Last Name:</label>
+                            <input type="text" class="form-control" id="last-name" style="width:50%" placeholder="Last Name...">
+                        </div>
+                        <div class="form-group">
+                            <label for="address">Address:</label>
+                            <input type="text" class="form-control" id="address" style="width:50%" placeholder="Address...">
+                        </div>
+                        <div class="form-group">
+                            <label for="city">City:</label>
+                            <input type="text" class="form-control" id="city" style="width:50%" placeholder="City...">
+                        </div>
+                        <div class="form-group">
+                            <label for="province">Province:</label>
+                            <select class="form-control" id="province" style="width:50%">
+                                <option value="AB">Alberta</option>
+                                <option value="BC">British Columbia</option>
+                                <option value="MB">Manitoba</option>
+                                <option value="NB">New Brunswick</option>
+                                <option value="NL">Newfoundland and Labrador</option>
+                                <option value="NT">Northwest Territories</option>
+                                <option value="NS">Nova Scotia</option>
+                                <option value="NU">Nunavut</option>
+                                <option value="ON" selected="selected">Ontario</option>
+                                <option value="PE">Prince Edward Island</option>
+                                <option value="QC">Quebec</option>
+                                <option value="SK">Saskatchewan</option>
+                                <option value="YT">Yukon</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="postal-code">Postal Code:</label>
+                            <input type="text" class="form-control" id="postal-code" style="width:50%" placeholder="Postal Code...">
+                        </div>
+                        <div class="form-group">
+                            <label for="phone-number">Phone Number:</label>
+                            <input type="text" class="form-control" id="phone-number" style="width:50%" placeholder="Phone Number...">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email:</label>
+                            <input type="text" class="form-control" id="email" style="width:50%" placeholder="Email...">
+                        </div>
+                        <div class="form-group">
+                            <label for="date-avail">Date Available:</label>
+                            <input type="text" class="form-control" id="date-avail" style="width:50%" placeholder="Date Available...">
+                        </div>
+                        <div class="form-group">
+                            <label for="position">Position Applied For:</label>
+                            <select class="form-control" id="position" style="width:50%">
+                                <option value="PM">Property Maintenance</option>
+                                <option value="LC">Landscape Construction</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="date-avail">Date Available:</label>
+                            <input type="text" class="form-control" id="date-avail" style="width:50%" placeholder="Date Available...">
+                        </div>
+                        <div class="form-group">
+                            <label for="can-cit">Are you a Canadian Citizen?:</label>
+                            <input type="checkbox" id="can-cit">
+                        </div>
+                        <div class="form-group">
+                            <label for="can-work">If no, are you authorized to work in Canada?:</label>
+                            <input type="checkbox" id="can-work">
+                        </div>
+                        <div class="form-group">
+                            <label for="prev-work">Have you ever worked for this company?:</label>
+                            <input type="checkbox" id="prev-work">
+                        </div>
+                        <div class="form-group">
+                            <label for="prev-work-time">If so, when?:</label>
+                            <input type="text" class="form-control" id="prev-work-time" style="width:50%" placeholder="Previously worked...">
+                        </div>
+                        <div class="form-group">
+                            <label for="felony">Have you been convicted of a felony?:</label>
+                            <input type="checkbox" id="felony">
+                        </div>
+                        <div class="form-group">
+                            <label for="felony-expl">If yes, explain?:</label>
+                            <input type="text" class="form-control" id="felony-expl" style="width:50%" placeholder="Explain...">
+                        </div>
+                        <div class="form-group">
+                            
+                        </div>
+                        <div class="form-group">
+                            
+                        </div>
+                        <div class="form-group">
+                            
+                        </div> 
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <p>Copyright &copy; Your Website 2014</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
